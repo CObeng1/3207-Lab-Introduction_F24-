758e94dab68c2dde1469778833f9fad49a34ec2e
Caitlyn Obeng <124214038+CObeng1@users.noreply.github.com>
2024-09-01 19:51:21 -0400

    Update random.c

--- random.c
+++ random.c
@@ -1,9 +1,16 @@
-// unedited
-char* rand_string_alloc(size_t size)
-{
-     char *s = malloc(size + 1);
-     if (s) {
-         rand_string(s, size);
-     }
-     return s;
+// randchar()
+// returns a random character from 'A-Z'. implement code into 
+// main program.
+
+char randchar() {
+    // generate a random number between 0 - 25
+    int random_number = rand() % 26;
+
+    // converting to a character in range a - z
+    return 'A' + random_number;
+}
+
+//initializing function to seed random num generator
+void init_random() {
+    strand(time(NULL));
 }
