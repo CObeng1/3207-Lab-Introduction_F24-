91f9db9fa6eb96b562d34572085735901e199e2a
Caitlyn Obeng <124214038+CObeng1@users.noreply.github.com>
2024-09-01 19:52:00 -0400

    Update print_random.c

--- print_random.c
+++ print_random.c
@@ -3,7 +3,10 @@ char* rand_string_alloc(size_t size)
 {
      char *s = malloc(size + 1);
      if (s) {
-         rand_string(s, size);
+        for(size_t i = 0; i < size; i++) {
+            s[i] = randchar();
+        }
+        s[size] = '\0'
      }
      return s;
 }
